ac2cf1d5d59b161c139b10170a5ced9e9148b150
Linbeannie <55075175+Linbeannie@users.noreply.github.com>
2020-12-23 23:58:26 -0700

    Create Balanceable.java

---
+++ src/Balanceable.java
@@ -0,0 +1,11 @@
+/*
+    Title: Balanceable .java
+    Author: Madeleine Macaulay
+    Email: mmacaulay@csumb.edu
+    Abstract: Balance interface
+    Date: 10/21/2020
+ */
+public interface Balanceable {
+    public double getBalance();
+    public void setBalance(double amount);
+}
