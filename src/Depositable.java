061cbb9ae9a6e59d582c799c5a57a7f09352c886
Linbeannie <55075175+Linbeannie@users.noreply.github.com>
2020-12-23 23:59:02 -0700

    Create Depositable.java

---
+++ src/Depositable.java
@@ -0,0 +1,10 @@
+/*
+    Title: Depositable.java
+    Author: Madeleine Macaulay
+    Email: mmacaulay@csumb.edu
+    Abstract: Deposit interface
+    Date: 10/21/2020
+ */
+public interface Depositable {
+    public void deposit(double amount);
+}
