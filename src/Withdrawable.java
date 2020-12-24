38c6f3a98e9314143238a2a6292aae48cb292133
Linbeannie <55075175+Linbeannie@users.noreply.github.com>
2020-12-23 23:59:47 -0700

    Create Withdrawable.java

---
+++ src/Withdrawable.java
@@ -0,0 +1,10 @@
+/*
+    Title: Withdrawable.java
+    Author: Madeleine Macaulay
+    Email: mmacaulay@csumb.edu
+    Abstract: Withdraw interface
+    Date: 10/21/2020
+ */
+public interface Withdrawable {
+    public void withdraw(double amount);
+}
